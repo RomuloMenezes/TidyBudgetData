9e82c10b17ed0c420604fb69dff5bfed2c7a667d
RomuloMenezes <romulo.menezes@gmail.com>
2016-11-21 11:59:01 -0200

    Output file generate WITHOUT TYPES OF ASSET.

--- TidyBudgetData/frmMain.cs
+++ TidyBudgetData/frmMain.cs
@@ -92,121 +92,212 @@ namespace TidyBudgetData
                 tblProjTypeOfAsset.Columns.Add("Desc", typeof(string));
                 tblProjTypeOfAsset.Columns.Add("Type", typeof(string));
 
-                System.Data.DataTable tblActualExpenses = new System.Data.DataTable();
-                tblActualExpenses.Columns.Add("Project", typeof(string));
-                tblActualExpenses.Columns.Add("Action", typeof(string));
-                tblActualExpenses.Columns.Add("Year", typeof(DateTime));
-                tblActualExpenses.Columns.Add("Value", typeof(float));
-
                 // Delete file if it exists, and create a new, empty one
                 if (File.Exists("D:\\_GIT\\Projetos\\GIT - Orçamento\\Plano de Ação\\TidyData.xlsx"))
                 {
-                    File.Delete("D:\\_GIT\\Projetos\\GIT - Orçamento\\Plano de Ação\\TidyData.xlsx");
-                }
-
-                xlTargetWorkBook = xlTargetApp.Workbooks.Add();
-                xlTargetWorkBook.SaveAs("D:\\_GIT\\Projetos\\GIT - Orçamento\\Plano de Ação\\TidyData.xlsx");
-
-                // Reading worksheet that relates projects / actions to types of asset
-                textBox4.Text = "Lendo tipos de ativo.";
-                textBox4.Refresh();
-
-                xlTypesOfAssetWorkBook = xlSourceApp.Workbooks.Open(textBox1.Text);
-                xlTypesOfAssetWorkSheet = xlTypesOfAssetWorkBook.Worksheets[1];
-
-                for (iRowIndex = 1; iRowIndex <= xlTypesOfAssetWorkSheet.UsedRange.Rows.Count; iRowIndex++)
-                {
-                    sCurrProj = xlTypesOfAssetWorkSheet.Cells[iRowIndex, 1].Value;
-                    if(sCurrProj.Substring(2,1)=="_")   
+                    sErrorMessage = "";
+                    try
                     {
-                        sCurrType = xlTypesOfAssetWorkSheet.Cells[iRowIndex, 2].Value;
-                        tblProjTypeOfAsset.Rows.Add(sCurrProj.Substring(0, 5), sCurrProj.Substring(8), sCurrType);
+                        File.Delete("D:\\_GIT\\Projetos\\GIT - Orçamento\\Plano de Ação\\TidyData.xlsx");
+                    }
+                    catch
+                    {
+                        sErrorMessage = "O arquivo destino está em uso por outro aplicativo.";
+                        MessageBox.Show(sErrorMessage, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        xlSourceApp.Quit();
+                        xlTargetApp.Quit();
+                        textBox4.Text = "";
+                        textBox4.Refresh();
                     }
                 }
 
-                System.Data.DataTable tblBudget = new System.Data.DataTable();
-                tblBudget.Columns.Add("Project", typeof(string));
-                tblBudget.Columns.Add("Action", typeof(string));
-                tblBudget.Columns.Add("Year", typeof(DateTime));
-                tblBudget.Columns.Add("Value", typeof(float));
-
-                // Reading worksheet with the budgeted values
-                textBox4.Text = "Lendo itens orçados.";
-                textBox4.Refresh();
-
-                xlBudgetWorkBook= xlSourceApp.Workbooks.Open(textBox2.Text);
-                xlBudgetWorkSheet = xlBudgetWorkBook.Worksheets[1];
-                iNbOfYears = xlBudgetWorkSheet.UsedRange.Columns.Count - 2;
-
-                for (iRowIndex = 1; iRowIndex <= xlBudgetWorkSheet.UsedRange.Rows.Count; iRowIndex++)
+                if(sErrorMessage=="")
                 {
-                    sCurrCell = xlBudgetWorkSheet.Cells[iRowIndex, 1].Value;
-                    if (sCurrCell != null)
+                    xlTargetWorkBook = xlTargetApp.Workbooks.Add();
+                    xlTargetWorkBook.SaveAs("D:\\_GIT\\Projetos\\GIT - Orçamento\\Plano de Ação\\TidyData.xlsx");
+                    xlTargetWorkSheet = xlTargetWorkBook.Worksheets[1];
+
+                    // Reading worksheet that relates projects / actions to types of asset
+                    textBox4.Text = "Lendo tipos de ativo.";
+                    textBox4.Refresh();
+
+                    xlTypesOfAssetWorkBook = xlSourceApp.Workbooks.Open(textBox1.Text);
+                    xlTypesOfAssetWorkSheet = xlTypesOfAssetWorkBook.Worksheets[1];
+
+                    for (iRowIndex = 1; iRowIndex <= xlTypesOfAssetWorkSheet.UsedRange.Rows.Count; iRowIndex++)
                     {
-                        if (sCurrCell.Substring(2, 1) == "_" || sCurrCell.Substring(4, 1) == "-") // Either a project or an action
+                        sCurrProj = xlTypesOfAssetWorkSheet.Cells[iRowIndex, 1].Value;
+                        if(sCurrProj.Substring(2,1)=="_")   
                         {
-                            if (sCurrCell.Substring(2, 1) == "_") // It's a project
-                                sCurrProj = sCurrCell;
-                            else // It's an action
+                            sCurrType = xlTypesOfAssetWorkSheet.Cells[iRowIndex, 2].Value;
+                            tblProjTypeOfAsset.Rows.Add(sCurrProj.Substring(0, 5), sCurrProj.Substring(8), sCurrType);
+                        }
+                    }
+
+                    System.Data.DataTable tblBudget = new System.Data.DataTable();
+                    tblBudget.Columns.Add("Project", typeof(string));
+                    tblBudget.Columns.Add("Action", typeof(string));
+                    tblBudget.Columns.Add("Year", typeof(DateTime));
+                    tblBudget.Columns.Add("Value", typeof(double));
+                    tblBudget.Columns.Add("Treated", typeof(Boolean));
+
+                    // Reading worksheet with the budgeted values
+                    textBox4.Text = "Lendo itens orçados.";
+                    textBox4.Refresh();
+
+                    xlBudgetWorkBook= xlSourceApp.Workbooks.Open(textBox2.Text);
+                    xlBudgetWorkSheet = xlBudgetWorkBook.Worksheets[1];
+                    iNbOfYears = xlBudgetWorkSheet.UsedRange.Columns.Count - 2;
+
+                    for (iRowIndex = 1; iRowIndex <= xlBudgetWorkSheet.UsedRange.Rows.Count; iRowIndex++)
+                    {
+                        sCurrCell = xlBudgetWorkSheet.Cells[iRowIndex, 1].Value;
+                        if (sCurrCell != null)
+                        {
+                            if (sCurrCell.Substring(2, 1) == "_" || sCurrCell.Substring(4, 1) == "-") // Either a project or an action
                             {
-                                sCurrAction = sCurrCell;
-                                for (iYearIndex = 0; iYearIndex < iNbOfYears; iYearIndex++)
+                                if (sCurrCell.Substring(2, 1) == "_") // It's a project
+                                    sCurrProj = sCurrCell;
+                                else // It's an action
                                 {
-                                    iCurrYear = Convert.ToInt16(xlBudgetWorkSheet.Cells[1, 2 + iYearIndex].Value);
-                                    dtCurrDate = new DateTime(iCurrYear, 1, 1);
-                                    if (xlBudgetWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value != null)
-                                        dCurrValue = xlBudgetWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value;
-                                    else
-                                        dCurrValue = 0;
-                                    tblBudget.Rows.Add(sCurrProj, sCurrAction, dtCurrDate, dCurrValue);
+                                    sCurrAction = sCurrCell;
+                                    for (iYearIndex = 0; iYearIndex < iNbOfYears; iYearIndex++)
+                                    {
+                                        iCurrYear = Convert.ToInt16(xlBudgetWorkSheet.Cells[1, 2 + iYearIndex].Value);
+                                        dtCurrDate = new DateTime(iCurrYear, 1, 1);
+                                        if (xlBudgetWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value != null)
+                                            dCurrValue = xlBudgetWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value;
+                                        else
+                                            dCurrValue = 0;
+                                        tblBudget.Rows.Add(sCurrProj, sCurrAction, dtCurrDate, dCurrValue, false);
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                // Reading worksheet with the actual expenses
-                textBox4.Text = "Lendo itens realizados.";
-                textBox4.Refresh();
+                    System.Data.DataTable tblActualExpenses = new System.Data.DataTable();
+                    tblActualExpenses.Columns.Add("Project", typeof(string));
+                    tblActualExpenses.Columns.Add("Action", typeof(string));
+                    tblActualExpenses.Columns.Add("Year", typeof(DateTime));
+                    tblActualExpenses.Columns.Add("Value", typeof(double));
+                    tblActualExpenses.Columns.Add("Treated", typeof(Boolean));
 
-                xlActualExpensesWorkBook = xlSourceApp.Workbooks.Open(textBox3.Text);
-                xlActualExpensesWorkSheet = xlActualExpensesWorkBook.Worksheets[1];
-                iNbOfYears = xlActualExpensesWorkSheet.UsedRange.Columns.Count - 2;
+                    // Reading worksheet with the actual expenses
+                    textBox4.Text = "Lendo itens realizados.";
+                    textBox4.Refresh();
 
-                for (iRowIndex = 1; iRowIndex <= xlActualExpensesWorkSheet.UsedRange.Rows.Count; iRowIndex++)
-                {
-                    sCurrCell = xlActualExpensesWorkSheet.Cells[iRowIndex, 1].Value;
-                    if (sCurrCell != null)
+                    xlActualExpensesWorkBook = xlSourceApp.Workbooks.Open(textBox3.Text);
+                    xlActualExpensesWorkSheet = xlActualExpensesWorkBook.Worksheets[1];
+                    iNbOfYears = xlActualExpensesWorkSheet.UsedRange.Columns.Count - 2;
+
+                    for (iRowIndex = 1; iRowIndex <= xlActualExpensesWorkSheet.UsedRange.Rows.Count; iRowIndex++)
                     {
-                        if (sCurrCell.Substring(2, 1) == "_" || sCurrCell.Substring(4, 1) == "-") // Either a project or an action
+                        sCurrCell = xlActualExpensesWorkSheet.Cells[iRowIndex, 1].Value;
+                        if (sCurrCell != null)
                         {
-                            if (sCurrCell.Substring(2, 1) == "_") // It's a project
-                                sCurrProj = sCurrCell;
-                            else // It's an action
+                            if (sCurrCell.Substring(2, 1) == "_" || sCurrCell.Substring(4, 1) == "-") // Either a project or an action
                             {
-                                sCurrAction = sCurrCell;
-                                for (iYearIndex = 0; iYearIndex < iNbOfYears; iYearIndex++)
+                                if (sCurrCell.Substring(2, 1) == "_") // It's a project
+                                    sCurrProj = sCurrCell;
+                                else // It's an action
                                 {
-                                    iCurrYear = Convert.ToInt16(xlActualExpensesWorkSheet.Cells[1, 2 + iYearIndex].Value);
-                                    dtCurrDate = new DateTime(iCurrYear, 1, 1);
-                                    if (xlActualExpensesWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value != null)
-                                        dCurrValue = xlActualExpensesWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value;
-                                    else
-                                        dCurrValue = 0;
-                                    tblActualExpenses.Rows.Add(sCurrProj, sCurrAction, dtCurrDate, dCurrValue);
+                                    sCurrAction = sCurrCell;
+                                    for (iYearIndex = 0; iYearIndex < iNbOfYears; iYearIndex++)
+                                    {
+                                        iCurrYear = Convert.ToInt16(xlActualExpensesWorkSheet.Cells[1, 2 + iYearIndex].Value);
+                                        dtCurrDate = new DateTime(iCurrYear, 1, 1);
+                                        if (xlActualExpensesWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value != null)
+                                            dCurrValue = xlActualExpensesWorkSheet.Cells[iRowIndex, 2 + iYearIndex].Value;
+                                        else
+                                            dCurrValue = 0;
+                                        tblActualExpenses.Rows.Add(sCurrProj, sCurrAction, dtCurrDate, dCurrValue, false);
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                textBox4.Text = "";
-                textBox4.Refresh();
-                xlSourceApp.Quit();
-                xlTargetWorkBook.Save();
-                xlTargetApp.Quit();
-                Cursor.Current = Cursors.Default;
-                MessageBox.Show("Data tidied up", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    // Generating output file
+                    textBox4.Text = "Gerando arquivo de saída.";
+                    textBox4.Refresh();
+
+                    System.Data.DataRow[] rowCurrActualExpenseResult;
+                    double dCurrActualExpenseValue = 0;
+                    string sSelect = "";
+
+                    // Writing header
+                    xlTargetWorkSheet.Cells[1, 1].Value = "Projeto";
+                    xlTargetWorkSheet.Cells[1, 2].Value = "Ação";
+                    xlTargetWorkSheet.Cells[1, 3].Value = "Ano";
+                    xlTargetWorkSheet.Cells[1, 4].Value = "Orçado";
+                    xlTargetWorkSheet.Cells[1, 5].Value = "Realizado";
+
+                    for (iRowIndex = 0; iRowIndex < tblBudget.Rows.Count;iRowIndex++)
+                    {
+                        sCurrProj = tblBudget.Rows[iRowIndex].Field<string>(0);
+                        sCurrAction = tblBudget.Rows[iRowIndex].Field<string>(1);
+                        dtCurrDate = tblBudget.Rows[iRowIndex].Field<DateTime>(2);
+                        dCurrValue = tblBudget.Rows[iRowIndex].Field<double>(3);
+                        sSelect = "Project = '" + sCurrProj + "' AND Action = '" + sCurrAction + "' AND Year = '" + Convert.ToString(dtCurrDate).Substring(0,10) + "'";
+                        rowCurrActualExpenseResult = tblActualExpenses.Select(sSelect);
+                        if (rowCurrActualExpenseResult.GetLength(0) > 0)
+                        {
+                            dCurrActualExpenseValue = rowCurrActualExpenseResult[0].Field<double>(3);
+                            rowCurrActualExpenseResult[0]["Treated"] = true;
+                        }
+                        else
+                            dCurrActualExpenseValue = 0;
+
+                        // 2 must be added to iRowIndex because it's 0-based, and Worksheet.Cells is 1-based. Besides that, a header line has been added.
+                        xlTargetWorkSheet.Cells[iRowIndex + 2, 1].Value = sCurrProj;
+                        xlTargetWorkSheet.Cells[iRowIndex + 2, 2].Value = sCurrAction;
+                        xlTargetWorkSheet.Cells[iRowIndex + 2, 3].Value = dtCurrDate;
+                        xlTargetWorkSheet.Cells[iRowIndex + 2, 4].Value = dCurrValue;
+                        xlTargetWorkSheet.Cells[iRowIndex + 2, 5].Value = dCurrActualExpenseValue;
+                        tblBudget.Rows[iRowIndex]["Treated"] = true;
+                    }
+                
+                    sSelect = "Treated = 'false'";
+                    rowCurrActualExpenseResult = tblActualExpenses.Select(sSelect);
+                    if (rowCurrActualExpenseResult.GetLength(0) > 0)
+                    {
+                        foreach(System.Data.DataRow rowCurrRow in rowCurrActualExpenseResult)
+                        {
+                            // 2 must be added to iRowIndex because it's 0-based, and Worksheet.Cells is 1-based. Besides that, a header line has been added.
+                            xlTargetWorkSheet.Cells[iRowIndex + 2, 1].Value = rowCurrRow["Project"];
+                            xlTargetWorkSheet.Cells[iRowIndex + 2, 2].Value = rowCurrRow["Action"];
+                            xlTargetWorkSheet.Cells[iRowIndex + 2, 3].Value = rowCurrRow["Year"];
+                            xlTargetWorkSheet.Cells[iRowIndex + 2, 4].Value = 0;
+                            xlTargetWorkSheet.Cells[iRowIndex + 2, 5].Value = rowCurrRow["Value"];
+                            rowCurrRow["Treated"] = true;
+                            iRowIndex++;
+                        }
+                    }
+
+                    /* ============================ DEBUG ============================ */
+                    rowCurrActualExpenseResult = tblBudget.Select(sSelect);
+                    if (rowCurrActualExpenseResult.GetLength(0) > 0)
+                    {
+                        sSelect = "Treated = 'false'";
+                    }
+
+                    rowCurrActualExpenseResult = tblActualExpenses.Select(sSelect);
+                    if (rowCurrActualExpenseResult.GetLength(0) > 0)
+                    {
+                        sSelect = "Treated = 'false'";
+                    }
+                    /* ============================ DEBUG ============================ */
+
+                    textBox4.Text = "";
+                    textBox4.Refresh();
+                    xlSourceApp.Quit();
+                    xlTargetWorkBook.Save();
+                    xlTargetWorkBook.Close();
+                    xlTargetApp.Quit();
+                    Cursor.Current = Cursors.Default;
+                    MessageBox.Show("Arquivo de saída gerado com sucesso", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
         }
     }
